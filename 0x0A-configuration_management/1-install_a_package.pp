8d95803c90420b6b95556e5e8454da0a715f5823
Princeeze <Trade2uwin@gmail.com>
2023-10-31 13:17:19 +0100

    Removed necessary files

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-# Install flask from pip3
-
-package { 'flask==2.1.0':
-  ensure   => installed,
-  path     => '/usr/bin:/usr/sbin:/bin',
-  provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,7 +0,0 @@
-# execute a command with puppet
-
-exec {'kill `pgrep killmenow`':
-  path     => '/usr/bin:/bin:usr/sbin',
-  provider => shell,
-  onlyif   => 'test `pgrep killmenow`'
-}
